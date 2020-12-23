43807147663f067814b338b05581f09031eacb90
dniloash <dniloash@gmail.com>
2020-12-23 15:10:11 -0500

    setup update

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from numpy.distutils.core import setup, Extension
 
-ext1 = Extension(name = 'create_inter',
-                 sources = ['pywinter/create_inter.f90'])
+ext1 = Extension(name = 'KreatE_inter_m_f',
+                 sources = ['pywinter/KreatE_inter_m_f.f90'])
 
 setup(
   name = 'pywinter',
   packages = ['pywinter'],
   ext_modules = [ext1], # this must be the same as the name above
-  version = '1.1.2',
+  version = '1.2.1',
   description = 'Create WRF-WPS intermediate files',
   long_description=("Python lib for creating WRF-WPS intermediate files.\n\n "
                       "Documentation:\n\n"
@@ -15,7 +15,7 @@ setup(
   author = 'Danilo A Suarez H',
   author_email = 'dniloash@gmail.com',
   url = 'https://github.com/dniloash/Pywinter',
-  download_url = 'https://github.com/dniloash/Pywinter/tarball/1.1.2',
+  download_url = 'https://github.com/dniloash/Pywinter/tarball/1.2.1',
   keywords = ['Python3', 'WRF', 'WPS','Intermediate files'],
   classifiers = [],
   python_requires='>=3.6',
