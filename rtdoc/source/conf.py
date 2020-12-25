a95da5dcb23d7fb443bbf809f7bb45b1f6a4b66c
dniloash <dniloash@gmail.com>
2020-12-25 12:30:29 -0500

    update conf version

--- rtdoc/source/conf.py
+++ rtdoc/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, dniloash'
 author = 'dniloash'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.1'
+release = '1.2.2'
 
 
 # -- General configuration ---------------------------------------------------
