e5e62079607d2d7a16a2ff87949f9ab014ab21c9
dniloash <dniloash@gmail.com>
2021-02-08 10:14:39 -0500

    setup update

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ setup(
   name = 'pywinter',
   packages = ['pywinter'],
   ext_modules = [ext1], # this must be the same as the name above
-  version = '1.2.4',
-  description = 'Create WRF-WPS intermediate files',
-  long_description=("Python lib for creating WRF-WPS intermediate files.\n\n "
+  version = '2.0.0',
+  description = 'Read and Create WRF-WPS intermediate files',
+  long_description=("Python lib for reading/creating WRF-WPS intermediate files.\n\n "
                       "Documentation:\n\n"
                       "https://pywinter.readthedocs.io/en/latest/\n"),
   author = 'Danilo A Suarez H',
   author_email = 'dniloash@gmail.com',
   url = 'https://github.com/dniloash/Pywinter',
-  download_url = 'https://github.com/dniloash/Pywinter/tarball/1.2.4',
-  keywords = ['Python3', 'WRF', 'WPS','Intermediate files'],
+  download_url = 'https://github.com/dniloash/Pywinter/tarball/2.0.0',
+  keywords = ['Python', 'WRF', 'WPS','Intermediate files'],
   classifiers = [],
   python_requires='>=3.6',
 )
