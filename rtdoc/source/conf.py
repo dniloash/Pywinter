80fcdd6f99abfa8ff0747086f3298954117a1286
dniloash <65090943+dniloash@users.noreply.github.com>
2024-06-17 20:34:18 -0500

    Update conf.py

--- rtdoc/source/conf.py
+++ rtdoc/source/conf.py
@@ -12,7 +12,6 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 #sys.path.insert(0, os.path.abspath('_ext'))
 
@@ -51,7 +50,6 @@ exclude_patterns = []
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
